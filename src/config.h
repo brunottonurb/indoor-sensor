5e6725db6fcb2edf74570bcbb73446d05a81036b
Bruno Péjac <bruno.pejac@gmail.com>
2024-02-05 21:07:18 +0100

    Move secrets to secrets.h, put config on version control

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+#define OLED_RESET -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+#define BUTTON_PIN D7
+
+#define MEASUREMENT_INTERVAL 120000
+#define LOCATION_NAME "LivingRoom"
+#define DISPLAY_DURATION 5000
+#define GRAPH_DURATION 4 * 60 * 60 * 1000 // 4 hours in milliseconds
