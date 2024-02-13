ba975ae2a33dd3a5007dfbefc9e39d874d3fb3ed
Bruno Péjac <bruno.pejac@gmail.com>
2024-02-13 21:37:03 +0100

    Fix config

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 #define OLED_RESET -1  // Reset pin # (or -1 if sharing Arduino reset pin)
 #define BUTTON_PIN D7
 
-#define MEASUREMENT_INTERVAL 120000
+#define MEASUREMENT_INTERVAL (2 * 60 * 1000) // 2 minutes in milliseconds
 #define LOCATION_NAME "LivingRoom"
-#define DISPLAY_DURATION 5000
-#define GRAPH_DURATION 4 * 60 * 60 * 1000 // 4 hours in milliseconds
+#define DISPLAY_DURATION (5 * 1000) // 5 seconds in milliseconds
+#define GRAPH_DURATION (4 * 60 * 60 * 1000) // 4 hours in milliseconds
